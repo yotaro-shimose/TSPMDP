063ec2e89db32b4af0b308024e818c76b2934078
yotaro-shimose <ma-nu-@outlook.jp>
2021-02-11 15:30:59 +0000

    introduced lr_scheduler(implementation to log lr is yet to be done)

---
+++ tspmdp/modules/lr_scheduler.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+class LRScheduler(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, maximum_lr: float, warmup_steps: int = 4000):
+        super().__init__()
+
+        self.maximum_lr = tf.cast(maximum_lr, tf.float32)
+
+        self.warmup_steps = tf.cast(warmup_steps, tf.float32)
+
+    def __call__(self, step: tf.Tensor):
+        warmup = self.maximum_lr * step / self.warmup_steps
+        decay = self.maximum_lr * \
+            tf.math.rsqrt(step) / tf.math.rsqrt(self.warmup_steps)
+
+        return tf.where(step < self.warmup_steps, warmup, decay)
